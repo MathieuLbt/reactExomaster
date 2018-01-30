3dae62ef0c792d74aa4a4948b7408d32705972b1
mathieulubert <mathieulubert@hotmail.com>
2018-01-30 10:40:05 +0100

    #1

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ class App extends React.Component {
             seriesEpisodesList: []
         };
     }
-jbjbjbj
+
     componentDidMount() {
 
-        fetch('seriesList.json',{})
+        fetch('seriesList.json', {})
             .then(response => response.json())
             .then(seriesListDepuisFichier => {
                 this.setState({seriesList: seriesListDepuisFichier});
@@ -23,23 +23,35 @@ jbjbjbj
                 console.log(error);
             })
             .then(function () {
-                alert("j'ai fait ce que j'ai pu");
+                /* alert("j'ai fait ce que j'ai pu");*/
             });
+
+        fetch('seriesEpisodesList.json', {})
+            .then(response => response.json())
+            .then(seriesListDepuisFichier => {
+                this.setState({seriesEpisodesList: seriesListDepuisFichier});
+
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
 
     }
 
     render() {
         return (
             <div>
+                <input type="text" placeholder="Recherche une serie"/>
                 <ul>
                     {this.state.seriesList.length ?
                         this.state.seriesList.map(item => <li key={item.id}>{item.seriesName}</li>)
                         : <li>Loading...</li>
                     }
                 </ul>
+
             </div>
         )
     }
-}
+};
 
-export default App;
+        export default App;
